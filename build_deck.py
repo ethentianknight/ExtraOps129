b0f2d38e0711dbdadcf883f4db8423e5e8fc4735
Mark Davis <MarkDeeDavis@hotmail.com>
2026-09-05 20:50:01 -0700

    Fix male Tuxedo model validation

--- build_deck.py
+++ build_deck.py
@@ -16,7 +16,7 @@ def main():
     assets = root / "assets"
     deck = root / "deck"
     work = root / ".build/deck"
-    output = root / "dist/Extra-Ops-129-Steam-Deck-v0.3.1-alpha1"
+    output = root / "dist/Extra-Ops-129-Steam-Deck-v0.3.2-alpha1"
     if output.exists():
         shutil.rmtree(output)
     output.mkdir(parents=True)
