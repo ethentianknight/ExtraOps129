84b2acbde001c324711531fe48b6224e5807a5f6
Mark Davis <MarkDeeDavis@hotmail.com>
2026-09-05 22:46:57 -0700

    Release 1.0

--- build_deck.py
+++ build_deck.py
@@ -16,7 +16,7 @@ def main():
     assets = root / "assets"
     deck = root / "deck"
     work = root / ".build/deck"
-    output = root / "dist/Extra-Ops-129-Steam-Deck-v0.3.5-alpha1"
+    output = root / "dist/Extra-Ops-129-Steam-Deck-v1.0"
     if output.exists():
         shutil.rmtree(output)
     output.mkdir(parents=True)
